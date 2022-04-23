8522638c96a14ed3dc2616b5fbe10fc6a498b007
aniketpatidar <67000557+aniketpatidar@users.noreply.github.com>
2022-04-23 13:25:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.14%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int num) {
+           
+        unsigned mask = ~0;
+        
+        while(mask & num) 
+            
+            mask = (mask << 1);
+        
+        return (~num) ^ mask;
+        
+    }
+};
